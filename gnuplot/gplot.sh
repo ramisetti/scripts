0b709d3bbc07f31e7b5efac0bd046f1039b06cfb
S.B.Ramisetti <ramisettisrinivas@yahoo.com>
2017-12-07 16:59:34 +0000

    can plot csv files as well

--- gnuplot/gplot.sh
+++ gnuplot/gplot.sh
@@ -4,10 +4,10 @@
 # it particularly to save time to do repeating stuff
 # By default the script will plot all columns in the datafile
 
-if [ ${#@} -le 1 ]; then
+if [ ${#@} -lt 1 ]; then
     echo "Usage: $0 datafile col (optional)"
-    echo "e.g.: $0 calcite.dat"
-    echo "e.g.: $0 calcite.dat 3"
+    echo "e.g.: $0 data.dat"
+    echo "e.g.: $0 data.dat 3"
     exit
 fi
 
@@ -18,11 +18,13 @@ filename="${filename%.*}"
 
 # switch flag_csv on to plot csv files 
 flag_csv=0
-if [ $fext == "csv" ]; then flag_csv=1; fi
-
 # get max number of columns in the data file
 mul_col=1
 max_col=$(awk '!/^($|#|@)/{print NF; exit}' $ifile)
+if [ $fext == "csv" ]; then 
+    flag_csv=1;
+    max_col=$(awk -F',' '!/^($|#|@)/{print NF; exit}' $ifile)
+fi
 
 if [ ${#@} -eq 2 ]; then
     col=$2
@@ -37,13 +39,20 @@ fi
 
 # invoke gnuplot to plot the data
 gnuplot -p <<- EOF
-set print "-"
-set xlabel "Label"
-set ylabel "Label2"
-set title "Graph title"
-if ("$flag_csv"==1) set datafile separator ','
+#set print "-"
+set ylabel "Energy (kJ.mol)"
+set xlabel "Tip-surface separation (nm)"
+set title "Free energy profile"
+#set terminal pngcairo
+#set output 'fep.png'
 
-if ("$mul_col"==1) plot for [col=2:$max_col] "${ifile}" u 1:col w lp
-if ("$mul_col"==0) plot "${ifile}" u 1:$col w lp
+if ($flag_csv==1) set datafile separator ','
+
+if ($mul_col==1) {
+plot for [col=2:$max_col] '${ifile}' u 1:col w lp
+}
+if ($mul_col==0){
+plot '$ifile' u 1:2 w lp notitle
+}
 
 EOF
